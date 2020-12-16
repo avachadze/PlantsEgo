2355d80e8ce98b5b374f70354968c3fd5b57547c
Ana <avachadze19dw31@ikzubirimanteo.com>
2020-12-16 09:26:55 +0100

    Fixed double menu

--- resources/views/layouts/mainLayout.blade.php
+++ resources/views/layouts/mainLayout.blade.php
@@ -12,41 +12,48 @@
     <link rel="stylesheet" href="node_modules/animate.css/animate.css">
 
 </head>
-<nav class="navbar navbar-expand-lg navbar-light" id="nav">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-        <div class="navbar-toggler-icon"></div>
-    </button>
-    <a class="navbar-brand" href="/"><img src="img/logo.png" alt="" id="logo"></a>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-            </li>
-            @section('compatibility')
+@section ('photoHeader')
 
-            @show
-            <li class="nav-item">
-                <a class="nav-link" href="/contact">Contact Us</a>
-            </li>
-        </ul>
-        <ul class="nav navbar-nav navbar-right">
-            <li class="nav-item ">
-                <button type="submit" class="btn btn-outline-success mx-1" id="login">Sign in</button>
-            </li>
-            <li class="nav-item ">
+@show
+<header>
+
+    <nav class="navbar navbar-expand-lg navbar-light" id="nav">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+            <div class="navbar-toggler-icon"></div>
+        </button>
+        <a class="navbar-brand" href="/"><img src="img/logo.png" alt="" id="logo"></a>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+                </li>
+                @section ('compatibility')
+                
+                @show
+                <li class="nav-item">
+                    <a class="nav-link" href="/contact">Contact</a>
+                </li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li class="nav-item ">
+                    <button type="submit" class="btn btn-outline-success mx-1" id="login">Sign in</button>
+                </li>
+                <li class="nav-item ">
+                    <button type="submit" class="btn btn-success " id="register">Sign up</button>
+                </li>
+    </nav>
+
+    <div class="pos-f-t">
+        <div class="collapse" id="navbarToggleExternalContent">
+            <div class="bg-light p-4">
+                <h4 class="text-dark">Log in or register</h4>
+                <button type="submit" class="btn btn-outline-success" id="login">Sign in</button>
                 <button type="submit" class="btn btn-success " id="register">Sign up</button>
-            </li>
-</nav>
-<div class="pos-f-t">
-    <div class="collapse" id="navbarToggleExternalContent">
-        <div class="bg-light p-4">
-            <h4 class="text-dark">Log in or register</h4>
-            <button type="submit" class="btn btn-outline-success" id="login">Sign in</button>
-            <button type="submit" class="btn btn-success " id="register">Sign up</button>
 
+            </div>
         </div>
     </div>
-</div>
+</header>
 
 <body id="top">
 
