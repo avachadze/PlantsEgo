d623ab797c38046dced5868a640ed38388ea4055
CarlosTaboada <ctaboadafe19dw31@ikzubirimanteo.com>
2020-12-16 18:12:10 +0100

    Authentication first functions fixed

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,7 +11,7 @@
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
-                        <input type="text" class="form-control" value="user" id="role" name="role" required autocomplete="role">
+                        <input type="text" class="d-none form-control" value="user" id="role" name="role" required autocomplete="role">
 
                         <div class="form-group row">
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
