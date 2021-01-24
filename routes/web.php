1ff220766360ce56ef43a8d8e979118c4e9dc6d1
Urbil27 <urbiltxo@gmail.com>
2021-01-24 12:53:08 +0100

    problems fixed

--- routes/web.php
+++ routes/web.php
@@ -31,12 +31,18 @@ Route::group([
 
         Route::post('/plants/add', 'PlantsController@store');
 
-  
+    Route::view('/', 'pages/index');
     Route::post('/plants/add', '\App\Http\Controllers\PlantsController@store');
+    Route::post('/systems/add/corporative','\App\Http\Controllers\SystemsController@store');
+    Route::post('/systems/add/personal','\App\Http\Controllers\SystemsController@store');
     Route::get('/systems','\App\Http\Controllers\SystemsController@index');
+    Route::view('/systems/add', '/pages/addSystem');
+    Route::get('/systems/{id}', '\App\Http\Controllers\PlantsController@index');
+    Route::get('/systems/{id}/addplant', '\App\Http\Controllers\PlantsController@showStoreForm');
     Route::get('/systems/{id}/{plantid}/addsensor', '\App\Http\Controllers\SensorsController@showStoreForm');
-    Route::get('/{id}/{plantid}', '\App\Http\Controllers\PlantsController@show');
-    Route::get('/{id}/{plantid}/statistics', '\App\Http\Controllers\SensorsController@index');
+    Route::get('/systems/{id}/{plantid}', '\App\Http\Controllers\PlantsController@show');
+    Route::get('/systems/{id}/{plantid}/statistics', '\App\Http\Controllers\SensorsController@index');
+    Route::post('/system/sensors/add', '\App\Http\Controllers\SensorsController@store');
 
     Route::get('/contact', function () {
         return view('pages/contact');
@@ -45,4 +51,12 @@ Route::group([
     Auth::routes(['verify' => true]);
     Route::get('/redirect', 'DashboardController@redirect')->name('usrDashboard');
 
+});
+Auth::routes(['verify' => true]);
+
+    Route::get('switchLang/{lang}', 'LangController')->name('switchLang');
+
+    Route::view('/contact', 'pages/contact');
+
+    Route::view('/', 'pages/index');
 });
