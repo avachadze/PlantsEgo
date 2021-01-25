09867e8cf4eac1f741d66595289ce03e05ac6e85
Ana <avachadze19dw31@ikzubirimanteo.com>
2021-01-25 09:24:36 +0100

    manually merged

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,47 +1,66 @@
 @extends('layouts/mainLayout')
-@section('main')
 
+@section('main')
+    <script src="/jquery/contactValidation.js"></script>
 <section class="container my-5">
 
+    @if(Session::has('savedMessage'))
+        <div class="row alert alert-lightSuccess border border-success mt-4 animate__animated animate__backInUp">
+            <h3>{{ Session::get('savedMessage') }}</h3>
+        </div>
+    @endif
+
   <h2 class="h1-responsive font-weight-bold text-center my-5">{{__('messages.contactTitle')}}</h2>
+
   <p class="text-center w-responsive mx-auto pb-5">{{__('messages.contactText')}}</p>
+
   <div class="row">
+
     <div class="col-lg-5 mb-lg-0 mb-4">
       <div class="card">
-        <div class=" card-body">
-          <form action="">
+        <div class="card-body">
+          <form action="{{ route('storeContactMessage') }}" method="POST">
+
+              @csrf
+
             <div class="form-header blue accent-1">
-              <h3 class="mt-2">{{__('messages.contactWrite')}}</h3>
+              <h3 class="mt-2"><i class="fas fa-envelope"></i> {{__('messages.contactWrite')}}</h3>
             </div>
             <div class="md-form">
-              <input type="text" id="nameC" class="form-control" required autocomplete>
-              <label for="nameC">{{__('auth.name')}}</label>
+              <i class="fas fa-user prefix grey-text"></i>
+                <label for="nameC">{{__('auth.name')}}</label>
+              <input type="text" id="nameC" name="nameC" @auth value="{{ Auth::user()->name }}" @endauth class="form-control">
             </div>
             <div class="md-form">
-              <input type="email" id="contactEmail" class="form-control" required autocomplete>
+              <i class="fas fa-envelope prefix grey-text"></i>
+                <label for="emailC">Email</label>
+              <input type="text" id="emailC" name="emailC" @auth value="{{ Auth::user()->email }}" @endauth class="form-control">
               @error('email')
               <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
               </span>
               @enderror
-              <label for="emailC">Email</label>
-            </div>
-            <div>
-              <div class="md-form">
-                <input type="text" id="form-Subject" class="form-control">
-              </div>
-              <label for="form-Subject">{{__('messages.subject')}}</label>
             </div>
             <div class="md-form">
-              <textarea id="msg" class="form-control md-textarea" rows="3" required autocomplete"></textarea>
-              <label for="msg">{{__('messages.sendMessage')}}</label>
+              <i class="fas fa-tag prefix grey-text"></i>
+                <label for="form-Subject">{{__('messages.subject')}}</label>
+              <input type="text" id="form-Subject" name="form-Subject" class="form-control">
+            </div>
+            <div class="md-form">
+              <i class="fas fa-pencil-alt prefix grey-text"></i>
+                <label for="msg">{{__('messages.sendMessage')}}</label>
+              <textarea id="msg" name="msg" class="form-control md-textarea mb-3" rows="3"></textarea>
             </div>
             <div class="text-center">
               <button id="submitContact" class="btn btn-outline-primary waves-effec">{{__('messages.submit')}}</button>
+
             </div>
+
+            </form>
         </div>
-        </form>
       </div>
+
+
     </div>
     <div class="col-lg-7">
       <div id="map-container-section" class="z-depth-1-half map-container-section mb-4" style="height: 400px">
@@ -51,20 +70,21 @@
       <div class="row text-center">
         <div class="col-md-4">
           <a class="btn-floating blue accent-1">
-
+            <i class="fas fa-map-marker-alt"></i>
           </a>
           <p>Donostia, 20012</p>
           <p class="mb-md-0">Guipuzcoa</p>
         </div>
         <div class="col-md-4">
           <a class="btn-floating blue accent-1">
-
+            <i class="fas fa-phone"></i>
           </a>
           <p>943 89 92 92</p>
           <p class="mb-md-0">Mon - Fri, 8:00-22:00</p>
         </div>
         <div class="col-md-4">
           <a class="btn-floating blue accent-1">
+            <i class="fas fa-envelope"></i>
           </a>
           <p>plantsEGO@gmail.com</p>
           <p class="mb-0">Plants EGO</p>
@@ -73,5 +93,4 @@
     </div>
   </div>
 </section>
-<script src="/jquery/contactValidation.js"></script>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
-use App\Http\Controllers\UsersController;
-use App\Http\Controllers\CompaniesController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,42 +13,65 @@ use App\Http\Controllers\CompaniesController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-
-
-Route::get('administrate', function () {
-    return view('admin/administration');
-});
-Route::get('edit', function () {
-    return view('admin/edit');
-});
-//User modifier 
-Route::get('administrate', [UsersController::class, 'index']);
-Route::get('deleteUser/{id}', [UsersController::class, 'delete']);
-Route::get('restoreUser/{id}', [UsersController::class, 'restore']);
-Route::get('destroyUser/{id}', [UsersController::class, 'destroy']);
-Route::get('edit/{id}', [UsersController::class, 'updateRole']);
-//Company Modifier
-Route::post('createCompany', 'CompaniesController@create');
-Route::put('updateCompany/{id}', 'CompaniesController@update');
-Route::delete('deleteCompany/{id}', 'CompaniesController@delete');
-Route::get('restoreCompany/{id}', 'CompaniesController@restore');
-Route::get('destroyCompany/{id}', 'CompaniesController@destroy');
-
 Route::group([
     'middleware' => 'setLocale'
 ], function () {
-    Route::get('switchLang/{lang}', 'LangController@switchLang')->name('switchLang');
 
-    Route::resource('Dashboard', 'DashboardController')->only('show')->middleware('auth');
+    Route::group([
+        'middleware' => 'verified'
+    ], function () {
 
-    Route::get('/contact', function () {
-        return view('pages/contact');
-    });
+        Route::group(
+            ['middleware' => 'auth'],
+            function (){
 
-    Route::get('/', function () {
-        return view('pages/index');
-    });
+                Route::resource('Dashboard', 'DashboardController')->only('show');
+                Route::resource('Company', 'CompaniesController');
+
+                Route::view('/edit', 'admin/edit');
+
+                //User modifier 
+                Route::get('administrate', 'UsersController@index');
+                Route::get('deleteUser/{id}', 'UsersController@delete');
+                Route::get('restoreUser/{id}', 'UsersController@restore');
+                Route::get('destroyUser/{id}', 'UsersController@destroy');
+                Route::get('edit/{id}', 'UsersController@updateRole');
+
+                //Company Modifier
+                Route::post('createCompany', 'CompaniesController@create');
+                Route::put('updateCompany/{id}', 'CompaniesController@update');
+                Route::get('deleteCompany/{id}', 'CompaniesController@delete');
+                Route::get('restoreCompany/{id}', 'CompaniesController@restore');
+                Route::get('destroyCompany/{id}', 'CompaniesController@destroy');
+
+                Route::post('/plants/add', 'PlantsController@store');
+                Route::post('/message/create', 'ContactMessagesController@store')->name('storeContactMessage');
+                Route::get('/message', 'ContactMessagesController@index')->name('messages.list');
+                Route::get('/message/send/{id}', 'ContactMessagesController@mailResponse')->name('contactResponseSend');
+                Route::get('/message/{id}', 'ContactMessagesController@show')->name('messages.show');
+                Route::delete('/message/{id}', 'ContactMessagesController@destroy')->name('destroyMessage');
+            
+                Route::post('/plants/add', '\App\Http\Controllers\PlantsController@store');
+                Route::post('/systems/add/corporative','\App\Http\Controllers\SystemsController@store');
+                Route::post('/systems/add/personal','\App\Http\Controllers\SystemsController@store');
+                Route::get('/systems','\App\Http\Controllers\SystemsController@index');
+                Route::view('/systems/add', '/pages/addSystem');
+                Route::get('/systems/{id}', '\App\Http\Controllers\PlantsController@index');
+                Route::get('/systems/{id}/addplant', '\App\Http\Controllers\PlantsController@showStoreForm');
+                Route::get('/systems/{id}/{plantid}/addsensor', '\App\Http\Controllers\SensorsController@showStoreForm');
+                Route::get('/systems/{id}/{plantid}', '\App\Http\Controllers\PlantsController@show');
+                Route::get('/systems/{id}/{plantid}/statistics', '\App\Http\Controllers\SensorsController@index');
+                Route::post('/system/sensors/add', '\App\Http\Controllers\SensorsController@store');
+        });
+    
+        Route::get('/redirect', 'DashboardController@redirect')->name('usrDashboard');
+
+});
     Auth::routes(['verify' => true]);
-    Route::get('/redirect', 'DashboardController@redirect')->name('usrDashboard');
+
+    Route::get('switchLang/{lang}', 'LangController')->name('switchLang');
+
+    Route::view('/contact', 'pages/contact');
+
+    Route::view('/', 'pages/index');
 });
