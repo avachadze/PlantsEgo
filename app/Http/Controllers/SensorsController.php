3689e35af451795b30ee4f8a93330158ede560f4
Urbil Melin <urbiltxo@gmail.com>
2021-01-28 21:54:15 +0100

    forms validated in server side

--- app/Http/Controllers/SensorsController.php
+++ app/Http/Controllers/SensorsController.php
@@ -58,6 +58,12 @@ class SensorsController extends Controller
     }
     public function store(Request $request)
     {
+        $this->validate($request,[
+            'name'=>'required',
+            'topic' => 'required',
+            'plant_id'=>'required'
+            
+        ]);
         $sensorName = $request->name;
         $sensorTopic = $request->topic;
         $sensorPlantId = $request->plant_id;
--- resources/views/pages/systems.blade.php
+++ resources/views/pages/systems.blade.php
@@ -6,7 +6,7 @@
 @section('main')
 
 <button class="add" data-toggle="modal" data-target="#exampleModal">
-<img src="img/plus.png" class="plus"alt="">
+<img src="{{asset('img/plus.png')}}" class="plus"alt="">
 </button>
 <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
